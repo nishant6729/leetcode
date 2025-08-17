f5981aaa8e1761e9d1469f29ac229183f63d15b6
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-08-17 15:13:05 +0530

    Time: 5 ms (10.35%) | Memory: 12.8 MB (26.82%) - LeetSync

--- 867-new-21-game/new-21-game.cpp
+++ 867-new-21-game/new-21-game.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    double new21Game(int n, int k, int maxPts) {
-        vector<double> P(n + 1, 0.0);
-        //P[i] = probability of getting score = i
-        
-        P[0] = 1; //Since already initally she has score = 0, hence probability for scoring 0 is 1
-        
-        double currProbabSum = k > 0 ? 1 : 0;
-        
-        for (int i = 1; i <= n; i++) {
-            
-            P[i] = currProbabSum/maxPts;
-            
-            if(i < k) {
-                currProbabSum += P[i];
+    double new21Game(int n, int k, int M) {
+        vector<double> P(n+1,0.0);
+        P[0]=1.0; //initially 0 point to hai hi na 
+
+        double currProbSum= k>0 ? 1.0: 0.0; // suru me total sari probability ka sum
+
+        for(int i=1;i<=n;i++){ // n se chota mamla chahiye isliye sbka nikal lete hai
+            P[i]=currProbSum/M; // abhi ki probability nikal li 
+            if(i<k){
+                currProbSum+=P[i]; // jha tk ki prob Sum aage kaam aagega usko hi involve kro jo aage kaam nhi aaeaga like k or usse bde 
             }
-            
-            if(i - maxPts >= 0 && i-maxPts < k) {
-                currProbabSum -= P[i-maxPts];
+
+            if(i-M>=0 && i-M<k){
+                currProbSum-=P[i-M];
             }
-            
+
         }
-        return accumulate(P.begin() + k, P.end(), 0.0);
+        return accumulate(P.begin()+k,P.end(),0.0);
+
     }
 };
