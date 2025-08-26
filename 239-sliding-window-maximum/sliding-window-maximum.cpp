37629c5a177b43c668ad52bb5bf3a4c9f6dddbff
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-08-26 16:20:40 +0530

    Time: 73 ms (14.21%) | Memory: 153.2 MB (14.89%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> st;
-
+        priority_queue<pair<int,int>> pq;
         vector<int> ans;
-
-        
         for(int i=0;i<k-1;i++){
-            st.insert(nums[i]);
+            pq.push({nums[i],i});
         }
         int i=0;
         int j=k-1;
-
         int n=nums.size();
         while(j<n){
-            st.insert(nums[j]);
-            ans.push_back(*st.rbegin());
-            st.erase(st.find(nums[i]));            
+            pq.push({nums[j],j});
+            while(!pq.empty() && (pq.top().second < i || pq.top().second > j)) {
+                pq.pop();
+            }
+            ans.push_back(pq.top().first);
+
             i++;
             j++;
+
         }
         return ans;
     }
