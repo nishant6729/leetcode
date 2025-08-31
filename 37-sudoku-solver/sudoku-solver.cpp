a6bc9dd082f7600a7030799ac03d629f0954b26a
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-08-31 21:23:48 +0530

    Time: 307 ms (64.50%) | Memory: 8.7 MB (62.78%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     bool isValid(vector<vector<char>>& board,int i,int j,char d){
         for(int k=0;k<9;k++){
-            if(board[i][k]==d) return false;
-            if(board[k][j]==d) return false;
+            if(board[i][k]==d) return false;   // us row ke sare cols explore
+            if(board[k][j]==d) return false;    // us col ke sare rows explore
         }
 
         int startI=i/3*3;   // start row no of that particular grid
@@ -11,7 +11,7 @@ public:
 
         for(int k=0;k<3;k++){
             for(int l=0;l<3;l++){
-                if(board[startI+k][startJ+l]==d) return false;
+                if(board[startI+k][startJ+l]==d) return false;   // traverse through all the elements of that block 
             }
         }
         return true;
@@ -22,21 +22,21 @@ public:
                 if(board[i][j]=='.'){
                     for(char d='1';d<='9';d++){
                         if(isValid(board,i,j,d)){
-                            board[i][j]=d;
+                            board[i][j]=d;    // try all possible valid char for that particular cell 
 
-                            if(solve(board)==true) return true;
-
-                            board[i][j]='.';
+                            if(solve(board)==true) return true;   // if saatisfy all the cells that is complete board then true 
+ 
+                            board[i][j]='.';    // otherwise backtrack it to original . and try for diff combinations 
                          }
                     }
-                    return false;
+                    return false;  // . hai or true nhi mil paa rha 
                 }
                 
             }
-        }
-        return true;
+        } 
+        return true;    // . hi nhi hai agar hoti toh pehle hi true ya false mil jata  
     }
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        solve(board);   // chlo sara explore krenge 
     }
 };
