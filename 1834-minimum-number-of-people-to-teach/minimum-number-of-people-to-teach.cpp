59f45f70ed459df70ebbba26d7121fca23e2c931
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-09-11 02:54:43 +0530

    Time: 496 ms (5.88%) | Memory: 239.1 MB (5.88%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
@@ -1,39 +1,48 @@
 class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
-        unordered_set<int> sadUsers;
-
-        for(auto &friends : friendships) {
-            int u = friends[0] - 1; //convert this to - based indexing
-            int v = friends[1] - 1;
-
-            unordered_set<int> langSet(begin(languages[u]), end(languages[u]));
-            bool canTalk = false;
-            for(int lang : languages[v]) {
-                if(langSet.count(lang)) {
-                    canTalk = true;
+        int s=languages.size();
+        vector<set<int>> temp(s);
+        for(int i=0;i<s;i++){
+            set<int> st;
+            for(auto x:languages[i]){
+                st.insert(x);
+            }
+            temp[i]=st;
+        }
+        set<int> unhappy;
+        for(int i=0;i<friendships.size();i++){
+            int f1=friendships[i][0];
+            int f2=friendships[i][1];
+            f1--;
+            f2--;
+            if (f1 < 0 || f1 >= s || f2 < 0 || f2 >= s) continue;
+            bool flag=false;
+            for(auto x:temp[f1]){
+                if(temp[f2].find(x)!=temp[f2].end()){
+                    flag=true;
                     break;
                 }
             }
-
-            if(!canTalk) {
-                sadUsers.insert(u);
-                sadUsers.insert(v);
+            if(!flag){
+                unhappy.insert(f1);
+                unhappy.insert(f2);
             }
         }
-
-        //Find max known language among sad users
-        vector<int> language(n+1, 0);
-        int mostKnownLang = 0;
-
-        for(int user : sadUsers) {
-            for(int lang : languages[user]) {
-                language[lang]++;
-                mostKnownLang = max(mostKnownLang, language[lang]);
+        int total=unhappy.size();
+        if (unhappy.empty()) return 0; 
+        unordered_map<int,int> mp;
+        for(auto x:unhappy){
+            for(auto i:temp[x]){
+                mp[i]++;
             }
+            
         }
-
-        return sadUsers.size() - mostKnownLang;
-        
+        int ans=INT_MIN;
+        for(auto p:mp){
+            ans=max(ans,p.second);
+        }
+        int finalSolution=total-ans;
+        return finalSolution;
     }
 };
