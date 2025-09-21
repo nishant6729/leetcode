2e00947c68bc90c93ee3e1936ab8bccb5a330589
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-09-21 20:20:43 +0530

    Time: 366 ms (33.58%) | Memory: 410.6 MB (60.58%) - LeetSync

--- 2023-design-movie-rental-system/design-movie-rental-system.cpp
+++ 2023-design-movie-rental-system/design-movie-rental-system.cpp
@@ -1,63 +1,71 @@
 class MovieRentingSystem {
 public:
-    typedef pair<int, int> P;
-
-    unordered_map<int, set<P>> available; //movei -> set<{price, shop}>
-    unordered_map<int, set<P>> movieToShopPrice; //movie -> set<shop, price>
-    set<tuple<int, int, int>> rented; //{price, shop, movie}
-
+    unordered_map<int,set<pair<int,int>>> available;
+    set<tuple<int,int,int>> rented;
+    unordered_map<string,int> price_map;
     MovieRentingSystem(int n, vector<vector<int>>& entries) {
-        for(auto &entry : entries) {
-            int shop = entry[0];
-            int movie = entry[1];
-            int price = entry[2];
-            available[movie].insert({price, shop}); //log(n)
-            movieToShopPrice[movie].insert({shop, price}); //log(n)
+        for(auto& entry:entries){
+
+            int shop=entry[0];
+            int movie=entry[1];
+            int price=entry[2];
+
+            available[movie].insert({price,shop});    // sorted on the basis of price
+            string key = to_string(shop) + "#" + to_string(movie); // CHANGED
+            price_map[key]=price;
         }
     }
     
     vector<int> search(int movie) {
-        vector<int> result;
-        int count = 0;
-        if(available.count(movie)) { //O(1)
-            for(auto &[_, shop] : available[movie]) { //O(5)
-                result.push_back(shop);
+        int count=0;
+        if(available[movie].size()==0) return {};
+        vector<int> ans;
+        for(auto& p:available[movie]){
+            if(count<5){
+                ans.push_back(p.second);
                 count++;
-                if(count >= 5)
-                    break;
             }
+            else break;
+            
         }
-
-        return result;
+        return ans;
     }
     
     void rent(int shop, int movie) {
-        auto it   = movieToShopPrice[movie].lower_bound({shop, INT_MIN}); //log(n)
-        int price = it->second;
-
-        available[movie].erase({price, shop}); //log(n)
-        rented.insert({price, shop, movie}); //log(n)
+        string key = to_string(shop) + "#" + to_string(movie); // CHANGED
+        int price=price_map[key];
+        available[movie].erase({price,shop});
+        rented.insert({price,shop,movie});
     }
     
     void drop(int shop, int movie) {
-        auto it = movieToShopPrice[movie].lower_bound({shop, INT_MIN});
-        int price = it->second;
+        string key = to_string(shop) + "#" + to_string(movie); // CHANGED
+        int price=price_map[key];
+        available[movie].insert({price,shop});
 
-        available[movie].insert({price, shop});
-        rented.erase({price, shop, movie});
+        rented.erase({price,shop,movie});
     }
     
     vector<vector<int>> report() {
-        vector<vector<int>> result;
-        int count = 0;
+        vector<vector<int>> ans;
+        int count=0;
 
-        for(auto &[price, shop, movie] : rented) { //O(5)
-            result.push_back({shop, movie});
-            count++;
-            if(count >= 5)
-                break;
+        for(auto& p:rented){
+            if(count<5){
+                ans.push_back({ get<1>(p), get<2>(p) });
+                count++;
+            }
+            else break;
         }
-
-        return result;
+        return ans;
     }
 };
+
+/**
+ * Your MovieRentingSystem object will be instantiated and called as such:
+ * MovieRentingSystem* obj = new MovieRentingSystem(n, entries);
+ * vector<int> param_1 = obj->search(movie);
+ * obj->rent(shop,movie);
+ * obj->drop(shop,movie);
+ * vector<vector<int>> param_4 = obj->report();
+ */
