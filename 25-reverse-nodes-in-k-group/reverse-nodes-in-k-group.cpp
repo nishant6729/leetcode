272cad6e76886124b6c4a0667273de24de0cc51b
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-09-23 20:25:46 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (70.46%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -1,55 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    // Proper reverse that returns new head
-    ListNode* reverse(ListNode* head) {
-        ListNode* prev = nullptr;
-        ListNode* curr = head;
-        while (curr) {
-            ListNode* nxt = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nxt;
+    ListNode* reverse(ListNode* head,ListNode* end){
+        ListNode* prev=NULL;
+        ListNode* curr=head;
+        ListNode* nxt;
+        if(curr){
+            nxt=curr->next;
         }
-        return prev; // new head of reversed list
-    }
-
-    // Find kth node from current node
-    ListNode* KthNode(ListNode* temp, int k) {
-        while (temp && k > 1) {
-            temp = temp->next;
-            k--;
+        else nxt=NULL;
+        while(curr!=end){
+            curr->next=prev;
+            prev=curr;
+            curr=nxt;
+            if(curr) nxt=nxt->next;
         }
-        return temp;
+        return prev;
     }
-
     ListNode* reverseKGroup(ListNode* head, int k) {
-        if (!head || k == 1) return head;
-
-        ListNode* dummy = new ListNode(0, head);
-        ListNode* prevGroupEnd = dummy;
-        ListNode* temp = head;
-
-        while (temp) {
-            ListNode* kthNode = KthNode(temp, k);
-            if (!kthNode) break; // not enough nodes for reversal
-
-            ListNode* nextGroupStart = kthNode->next;
-            kthNode->next = nullptr;
-
-            // reverse current group
-            ListNode* newHead = reverse(temp);
-
-            // connect previous group with new head
-            prevGroupEnd->next = newHead;
-
-            // connect old head (temp, now tail) with next group
-            temp->next = nextGroupStart;
-
-            // update for next iteration
-            prevGroupEnd = temp;
-            temp = nextGroupStart;
+        deque<pair<ListNode*,ListNode*>> dq;
+        int size=0;
+        ListNode* temp=head;
+        while(temp){
+            size++;
+            temp=temp->next;
         }
+        int loop=size/k;
+        ListNode* start=head;
+        ListNode* end=start;
+        for(int i=0;i<loop;i++){
+            for(int j=0;j<k;j++){
+                if(end) end=end->next;
+            }
+            ListNode* first=reverse(start,end);
+            ListNode* second=start;
+            dq.push_back({first,second});
+            start=end;
+        }
+        if(start){
+            dq.push_back({start,NULL});
+        }
+        dq.push_back({NULL,NULL});
+        ListNode* dummy=new ListNode(0);
 
+        dummy->next=dq[0].first;
+        for(int i=0;i<dq.size()-1;i++){
+            if(dq[i].second) dq[i].second->next=dq[i+1].first;
+            
+
+
+        }
         return dummy->next;
     }
 };
