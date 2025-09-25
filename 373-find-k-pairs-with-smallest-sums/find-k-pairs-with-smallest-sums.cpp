3619c605f7af17856200d05607924156b2044316
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-09-25 20:45:33 +0530

    Time: 75 ms (58.26%) | Memory: 141.5 MB (55.41%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    typedef pair<int,pair<int,int>> P;
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        int n2=nums2.size();
-        int n1=nums1.size();
-        priority_queue<P,vector<P>,greater<P>> pq;
+        int m=nums1.size();
+        int n=nums2.size();
+        set<pair<int,int>> st;
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> >pq;
+
         pq.push({nums1[0]+nums2[0],{0,0}});
-        set<pair<int,int>> visited;
-        visited.insert({0,0});
+        st.insert({0,0});
         vector<vector<int>> ans;
-        while(k-- && !pq.empty()){
-            auto temp=pq.top();
+        while(!pq.empty() && k>0){
+            int i=pq.top().second.first;
+            int j=pq.top().second.second;
             pq.pop();
-            int i=temp.second.first;
-            int j=temp.second.second;
             ans.push_back({nums1[i],nums2[j]});
-            if(j+1<n2 && visited.find({i,j+1})==visited.end()){
-                pq.push({nums1[i]+nums2[j+1],{i,j+1}});
-                visited.insert({i,j+1});
+            k--;
+            if(i+1<m){
+                if(!(st.count({i+1,j})))
+                {
+                    pq.push({nums1[i+1]+nums2[j],{i+1,j}});
+                    st.insert({i+1,j});
+                }
             }
-            if(i+1<n1 && visited.find({i+1,j})==visited.end()){
-                pq.push({nums1[i+1]+nums2[j],{i+1,j}});
-                visited.insert({i+1,j});
+            if(j+1<n){
+                if(!(st.count({i,j+1})))
+               {     pq.push({nums1[i]+nums2[j+1],{i,j+1}});
+                    st.insert({i,j+1});}
             }
+
+
         }
+        
         return ans;
+
     }
 };
