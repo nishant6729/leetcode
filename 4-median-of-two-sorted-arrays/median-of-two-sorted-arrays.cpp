56d1da525e9983cecb29b12f70049c5a982ff5fd
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-09-25 20:01:02 +0530

    Time: 0 ms (100.00%) | Memory: 95 MB (80.55%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,37 +1,46 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        if(nums1.size()>nums2.size()) return findMedianSortedArrays(nums2,nums1);  // choti size wale pe kaam krna hai 
+        // classical level wala question 
+        // Binary search kis kis tarah se use ho skta btata hai 
+
+        if(nums1.size()>nums2.size()){ 
+            return findMedianSortedArrays(nums2,nums1);     // choti size wale pe binary search lgana hai 
+        }
         int m=nums1.size();
         int n=nums2.size();
 
-        int lo=0;  
-        int hi=m;  // BS on no of elements from nums1 in left direction
+        int lo=0;
+        int hi=m;    // kitne element left portion of final array me aaenge nums 1 se uska max
 
         while(lo<=hi){
-            int Px=lo+(hi-lo)/2;  // nums1 ke kitne element left portion me aaenge jispe hum kaam krne wale
+            int Px=lo+(hi-lo)/2;   // left portion me itne ele me liye nums1 se
+
+            int Qx=(m+n+1)/2-Px;
+
+            int x1=(Px==0)?INT_MIN:nums1[Px-1];  // left side me last ele of nums1
+
+            int x2=(Qx==0)?INT_MIN:nums2[Qx-1];  // left side me last ele from nums2
+
+            int x3=(Px==m)?INT_MAX:nums1[Px];       // sare left me chle gye toh right me kya hai
+
+            int x4=(Qx==n)?INT_MAX:nums2[Qx];
 
 
-            int Qx=(m+n+1)/2-Px ;// nums2 ke ele in left portion
-            //left side
-            int x1=(Px==0)?INT_MIN : nums1[Px-1];   //left side me koi nhi hai khali pda hai 
-            int x2=(Qx==0)?INT_MIN : nums2[Qx-1];
-            //right side
-            int x3=(Px==m) ? INT_MAX :nums1[Px];
-            int x4=(Qx==n) ? INT_MAX :nums2[Qx];
-
-            if(x1<=x4 && x2<=x3){
+            if(x1<=x4  && x2<=x3){   // shi bn rha hai sb kuch 
                 if((m+n)%2==0){
-                    return (max(x1,x2)+min(x3,x4))/2.0;     //left ka last and right ka first taki median calc ho ske
-                } 
+                    return (max(x1,x2)+min(x3,x4))/2.0;
+                }
                 else{
                     return max(x1,x2);
                 }
             }
-            else if(x1>x4){
-                hi=Px-1;   //jyda element le liye the left me 
+            if(x1>x4){   // jyda ele le liye nums1 se 
+                hi=Px-1;
             }
-            else lo=Px+1;
+            else{
+                lo=Px+1;  // km element pd gye nums1 se left side me or nums2 se jyda aa gye 
+            }
         }
         return -1;
     }
