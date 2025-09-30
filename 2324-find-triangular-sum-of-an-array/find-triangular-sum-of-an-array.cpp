4c232714a43d34bf7861f69af64c436ffa65ddd1
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-10-01 01:49:22 +0530

    Time: 363 ms (20.35%) | Memory: 382.3 MB (23.78%) - LeetSync

---
+++ 2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,19 @@
+//Approach-1 (Using O(n) space - Do as asked)
+//T.C : O(n^2)
+//S.C : O(n)
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+
+        while (nums.size() > 1) {
+            vector<int> temp;
+            for (int i = 0; i < nums.size() - 1; ++i) {
+                temp.push_back((nums[i] + nums[i + 1]) % 10);
+            }
+            //std::move - avoids copying and is faster and more memory-efficient.
+            nums = move(temp);
+        }
+
+        return nums[0];
+    }
+};
