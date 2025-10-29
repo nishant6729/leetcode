ecbc19a96408f3b0cf8e76a00958f725af2121fb
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-10-29 18:51:34 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (30.21%) - LeetSync

--- 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -2,9 +2,9 @@ class Solution {
 public:
     int smallestNumber(int n) {
         if(n<3) return 1;
-        vector<int> temp(20);
+        vector<int> temp(10);
         temp[0]=3;
-        for(int i=1;i<20;i++){
+        for(int i=1;i<10;i++){
             temp[i]=temp[i-1]+pow(2,i+1);
         }
         for(auto& i:temp){
