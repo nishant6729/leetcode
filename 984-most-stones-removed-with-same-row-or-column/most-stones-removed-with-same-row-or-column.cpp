6d38396aedb2c9576c4cad6c92d4a4757dcc4783
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-12-24 12:57:54 +0530

    Time: 39 ms (38.89%) | Memory: 18.4 MB (83.24%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,58 @@
+class DisjointSet{
+public:
+        int n;
+
+        vector<int> size;
+        vector<int> parent;
+        DisjointSet(int n){
+            size.resize(n+1,1);
+            parent.resize(n+1);
+
+            for(int i=0;i<=n;i++){
+                parent[i]=i;
+            }
+
+        }
+        int findParent(int n){
+            if(parent[n]==n) return n;
+            return parent[n]=findParent(parent[n]);
+        }
+        void DoUnion(int a,int b){
+            a=findParent(a);
+            b=findParent(b);
+
+            if(a==b)  return;
+
+            if(size[a]<=size[b]){
+                parent[a]=b;
+                size[b]+=size[a];
+            }
+            else{
+                parent[b]=a;
+                size[a]+=size[b];
+            }
+
+           
+        }
+};
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n=stones.size();  // kitne stones hai
+
+        DisjointSet ds(n+1);
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(stones[i][0]==stones[j][0] || stones[i][1]==stones[j][1]){
+                    ds.DoUnion(i,j);
+                }
+            }
+        }
+        int component=0;
+        for(int i=0;i<n;i++){
+            if(ds.findParent(i)==i) component++;
+        }
+        return n-component;
+    }
+};
