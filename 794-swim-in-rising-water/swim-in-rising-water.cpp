cadfd03cb5870005374e2fe1242dbae8e05c75b0
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2025-12-25 11:50:19 +0530

    Time: 3 ms (91.44%) | Memory: 12.7 MB (93.67%) - LeetSync

--- 794-swim-in-rising-water/swim-in-rising-water.cpp
+++ 794-swim-in-rising-water/swim-in-rising-water.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    int n;
-    vector<vector<int>> directions{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    bool visited[50][50];
-    bool reachable(vector<vector<int>>& grid, int i, int j, int mid) {
-        if(i < 0 || i >= n || j < 0 || j >= n || visited[i][j] || grid[i][j] > mid)
-            return false;
-        
-        visited[i][j] = true;
-        
-        if(i == n-1 && j == n-1)
-            return true;
-        
-        for(vector<int> &dir : directions) {
-            int new_i = i + dir[0];
-            int new_j = j + dir[1];
-            
-            if(reachable(grid, new_i, new_j, mid))
-                return true;
-        }
-        
-        return false;
-    }
-    
+    vector<vector<int>> dir={{-1,0},{1,0},{0,1},{0,-1}};
     int swimInWater(vector<vector<int>>& grid) {
-        n = grid.size();
-        
-        int l = grid[0][0], r = n*n-1;
-        int result = 0;
+        // 
+        int n=grid.size();
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>> >pq;
+        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
+         dist[0][0]=grid[0][0];
+        pq.push({grid[0][0],{0,0}});
 
-        while(l <= r) {
-            int mid = l + (r-l)/2;
-            memset(visited, 0, sizeof(visited));
-            
-            if(reachable(grid, 0, 0, mid)) {
-                result = mid;
-                r = mid-1;
-            } else {
-                l = mid+1;
+        while(!pq.empty()){
+            auto it=pq.top();
+            pq.pop();
+            int currTime=it.first;
+
+            int i=it.second.first;
+            int j=it.second.second;
+
+            if(currTime>dist[i][j]) continue;
+
+           
+
+            if(i==n-1 && j==n-1){
+                return currTime;
+            }
+            for(auto& d:dir){
+                int newI=i+d[0];
+                int newJ=j+d[1];
+
+                
+                if(newI>=0 && newI<n && newJ>=0 && newJ<n){
+                    int newTime=max(currTime,grid[newI][newJ]);
+                    if(newTime < dist[newI][newJ]){
+                        dist[newI][newJ] = newTime;  
+                        pq.push({newTime,{newI,newJ}});
+                    }
+                }
             }
         }
-        
-        return result;
+        return -1;
     }
 };
