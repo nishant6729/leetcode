1411b8f3e7460643972f22ed5296e2e5e29e9baf
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-01-20 19:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (7.40%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,21 +1,48 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int n=matrix.size();
-        int m=matrix[0].size();
-        int i=0;
-        int j=m-1;
-        while(i<n && j<m){
-            if((j>=0 && i<n) && matrix[i][j]==target){
+    bool BinarySearch(int rowM,vector<vector<int>>& matrix,int target){
+        int lo=0;
+        int hi=matrix[0].size()-1;
+
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+
+            if(matrix[rowM][mid]==target){
                 return true;
             }
-            else if((j>=0 && i<n) &&matrix[i][j]>target){
-                j--;
+            else if(matrix[rowM][mid]>target){
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+
+        return false;
+    }
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        int rowL=0;
+        int rowH=m-1;
+
+        int colH=n-1;
+        int colL=0;
+
+        while(rowL<=rowH){
+            int rowM=rowL+(rowH-rowL)/2;
+
+
+            if(matrix[rowM][colL]>target){
+                rowH=rowM-1;
+            }
+            else if(matrix[rowM][colH]<target){
+                rowL=rowM+1;
             }
             else{
-                i++;
+                return BinarySearch(rowM,matrix,target);
             }
         }
+
         return false;
     }
 };
