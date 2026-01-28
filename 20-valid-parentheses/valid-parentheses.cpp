723af997a077c700b46070435c62a4260fc7b0dd
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-01-28 23:27:45 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.98%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     bool isValid(string s) {
-        
         stack<char> st;
-        for(char& c:s){
 
+        for(auto& c:s){
             if(c=='(' || c=='{' || c=='['){
+
                 st.push(c);
             }
             else{
+
+
                 if(c==')'){
-                    if(st.empty()) return false;
-                    if(st.top()!='(') return false;
-                    else st.pop();
+                    if(st.empty() || st.top()!='(') return false;
+                    st.pop();
                 }
-                else if(c=='}'){
-                    if(st.empty()) return false;
-                    if(st.top()!='{') return false;
-                    else st.pop();
+                else if(c==']'){
+                     if(st.empty() || st.top()!='[') return false;
+                     st.pop();
                 }
                 else{
-                    if(st.empty()) return false;
-                    if(st.top()!='[') return false;
-                    else st.pop();
+                    if(st.empty() || st.top()!='{') return false;
+                    st.pop();
                 }
             }
         }
