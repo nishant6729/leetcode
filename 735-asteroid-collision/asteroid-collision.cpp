a6bd0e663f2e42ab9a498ba0598a3213658b2988
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-01-29 20:05:26 +0530

    Time: 4 ms (14.99%) | Memory: 22.1 MB (55.97%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -9,19 +9,19 @@ public:
 
         for(int i=0;i<asteroids.size();i++){
             if(asteroids[i]>0){
-                st.push(asteroids[i]);
+                st.push(asteroids[i]);  // hum to right me chle
             }
 
             else{
-                while(!st.empty() && (st.top()>0) && st.top()<abs(asteroids[i])){
+                while(!st.empty() && (st.top()>0) && st.top()<abs(asteroids[i])){ // mere dushman + chote log ko maro
                     st.pop();
                 }
 
-                if(!st.empty() && st.top()==abs(asteroids[i])){
+                if(!st.empty() && st.top()==abs(asteroids[i])){ // marta hua aaya or mera bhai same value wala mila toh me khud bhi mra isliye woh pop and agli condition me else if kyuki ab me push nhi ho skta kyuki me bhi mr gya na 
                     st.pop();
                 }
 
-                else if(st.empty() || st.top()<0){
+                else if(st.empty() || st.top()<0){ // ya toh koi bcha nhi ya meri jaati wale log hai yani me bcha rhuga akhir tk kyuki aane wale ya toh meri jaati ke honge ya agr dusri jaaati ke honge toh dusri side jaenge
                     st.push(asteroids[i]);
                 }           
             }
