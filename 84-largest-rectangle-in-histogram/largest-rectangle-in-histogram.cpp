cb39aa192b7ab66b061f818f0172af0dd5d7a640
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-01-30 00:08:42 +0530

    Time: 31 ms (28.67%) | Memory: 85.8 MB (24.68%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -1,34 +1,65 @@
 class Solution {
 public:
-    int largestRectangleArea(vector<int>& heights) {
-        int n=heights.size();
-        vector<int> nsi(n);
-        vector<int> psi(n);
-        psi[0]=-1;
+    vector<int> helper1(vector<int>& heights){
         stack<int> st;
-        st.push(0);
-        for(int i=1;i<n;i++){
+        
+        int n=heights.size();
+
+        vector<int> temp(n);
+
+        temp[n-1]=n;
+
+        st.push(n-1);
+
+        for(int i=n-2;i>=0;i--){
             while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
-            if(st.empty()) psi[i]=-1;
-            else psi[i]=st.top();
+            if(st.empty()) temp[i]=n;
+            else temp[i]=st.top();
             st.push(i);
         }
-        stack<int> gt;
-        gt.push(n-1);
-        nsi[n-1]=n;
-        for(int i=n-2;i>=0;i--){
-            while(!gt.empty() && heights[gt.top()]>=heights[i]) gt.pop();
-            if(gt.empty()) nsi[i]=n;
-            else nsi[i]=gt.top();
-            gt.push(i);
+
+        return temp;
+
+    }
+    vector<int> helper2(vector<int>& heights){
+        stack<int> st;
+        
+        int n=heights.size();
+
+        vector<int> temp(n);
+
+        temp[0]=-1;
+
+        st.push(0);
+
+        for(int i=1;i<n;i++){
+            while(!st.empty() && heights[st.top()]>=heights[i]) st.pop();
+            if(st.empty()) temp[i]=-1;
+            else temp[i]=st.top();
+            st.push(i);
         }
+
+        return temp;
+
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        // Yaad rkhna smaller indexes aate hai use me greater nhi 
+        int n=heights.size();
+
+
+        vector<int> nsi=helper1(heights);
+        vector<int> psi=helper2(heights);
+
         int maxArea=0;
+
         for(int i=0;i<n;i++){
-            int height=heights[i];
-            int width;
-            width=nsi[i]-psi[i]-1;
-            maxArea=max(maxArea,height*width);
+            int currHeight=heights[i];
+
+            int currWidth=nsi[i]-psi[i]-1;  // dono ko include nhi isliye -1
+
+            maxArea=max(maxArea,currHeight*currWidth);
         }
+
         return maxArea;
     }
 };
