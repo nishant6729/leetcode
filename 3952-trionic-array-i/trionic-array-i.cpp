b75e5651764f63d35d905d0e85fc1c75fce1a787
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-02-04 01:15:41 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (70.12%) - LeetSync

---
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int count=0;
+        bool increasing=true;
+        int n=nums.size();
+        if(nums[n-1]==nums[n-2]) return false;
+        for(int i=1;i<nums.size()-1;i++){
+
+            if(nums[i]==nums[i-1]) return false;
+            
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                if(increasing){
+                    count++;
+                    increasing=false;
+                }
+                else return false;
+            }
+            else if(nums[i]<nums[i-1] && nums[i]<nums[i+1]){
+                if(!increasing){
+                    count++;
+                    increasing=true;
+                }
+                else return false;
+            }
+        }
+
+        return count==2;
+    }
+};
