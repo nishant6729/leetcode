756baf3e691dd6eb2c975a93d78a763725752bd2
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-02-06 16:25:23 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (83.63%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
-    vector<int> arr;
-    int n;
-    int dp[10001];
-    int helper(int idx){
-        if(idx==n-1) return 0;
-        if(idx>=n){
-            return INT_MAX;
-        }
-        if(dp[idx]!=-1) return dp[idx];
-        int curr=arr[idx];
-
-        if(curr==0) return INT_MAX;
-        if(idx+curr>=n-1) return 1;
-        int ans=INT_MAX;
-        for(int i=1;i<=curr;i++){
-            ans=min(ans,helper(idx+i));
-        }
-        if(ans==INT_MAX) return INT_MAX;
-        return dp[idx]=1+ans;
-    }
     int jump(vector<int>& nums) {
-        arr=nums;
-        n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        return helper(0);
+        int jumps=0;
+
+        int l=0;
+        int r=0;
+
+        while(r<nums.size()-1){
+            int farthest=0;
+
+            for(int i=l;i<=r;i++){  // range ke sare logo ko dekho 
+                farthest=max(farthest,i+nums[i]);
+            }
+
+            l=r+1;
+            r=farthest;
+            jumps++;
+        }
+
+        return jumps;
     }
 };
