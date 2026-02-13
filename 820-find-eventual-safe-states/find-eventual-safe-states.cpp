7c02d9512a3ee915697d3330526bc43f661df0b5
Nishant Gurjar <150243112+nishant6729@users.noreply.github.com>
2026-02-13 20:57:43 +0530

    Time: 76 ms (6.97%) | Memory: 72.9 MB (5.07%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,57 +1,53 @@
 class Solution {
 public:
-    vector<vector<int>> adj;
-    int dfs(int node,vector<int>& ans,unordered_set<int>& visited){
-        if(ans[node]==1){
-            return 1;
-        }
-        if(visited.find(node)!=visited.end()){
-            ans[node]=0;
-            return 0;
-        }
-        visited.insert(node);
-        for(auto& neigh:adj[node]){
-            if(!dfs(neigh,ans,visited)){
-                return ans[node]=0;
-            }
-        }
-
-        return ans[node]=1;
-
-    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        adj=graph;
+
         int n=graph.size();
 
-        vector<int> ans(n,-1);
+        unordered_map<int,vector<int>> mp;
+        vector<int> outdegree(n,-1);
+
+        for(int i=0;i<n;i++){
+            outdegree[i]=graph[i].size();
+
+            for(auto& ele:graph[i]){
+                mp[ele].push_back(i);  // reversed the edges
+            }
+        }
+
+        queue<int> qu;
+
+        for(int i=0;i<n;i++){
+            if(outdegree[i]==0){
+                qu.push(i);
+
+                
+            }
+        }
+        vector<int> ans;
+        while(!qu.empty()){
+            auto it=qu.front();
+
+            qu.pop();
+
+            ans.push_back(it);
+
+            for(auto& neigh:mp[it]){
+                outdegree[neigh]--;
+                if(outdegree[neigh]==0){
+                    qu.push(neigh);
+
+                }
+            }
+
+
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
 
         
+        
 
-       
-
-        for(int i=0;i<n;i++){
-            vector<int> nodes=graph[i];
-            if(nodes.size()==0){
-                ans[i]=1;
-            }
-        }
-
-        for(int i=0;i<n;i++){
-
-            if(ans[i]==-1){
-                unordered_set<int> visited;
-                ans[i]=dfs(i,ans,visited);
-            }
-            
-            
-        }
-        vector<int> result;
-        for(int i=0;i<ans.size();i++){
-            if(ans[i]==1){
-                result.push_back(i);
-            }
-        }
-
-        return result;
+        
     }
 };
